82ce0c1b2b3cda14be10673a614a65ffda795fe3
Ibne Zabed <ibnezabed@Ibnes-MacBook-Pro.local>
2018-03-24 15:47:51 -0400

    Added comments

--- python-selenium/tests/login_test.py
+++ python-selenium/tests/login_test.py
@@ -8,16 +8,22 @@ class TestLogin():
     # In pytest we do this with a fixture (function gets called before test and also after)
     # Method driver
     @pytest.fixture()
+    # self is a required parameter for class methods
+    # request is a parameter made available to fixtures. It enables access to loads of things during a test run
     def driver(self, request):
         #_geckodriver = os.path.join(os.getcwd(), 'vendor', 'geckodriver')
-        driver = webdriver.Firefox(executable_path='/Users/ibnezabed/Desktop/selenium-guidebook-python-practice/python-selenium/vendor/geckodriver')
-
+        #driver_ = webdriver.Firefox(executable_path=_geckodriver)
+        driver_ = webdriver.Firefox(executable_path='/Users/ibnezabed/Desktop/selenium-guidebook-python-practice/python-selenium/vendor/geckodriver')
+        # part of method driver
         def quit():
-            driver.quit()
-
+            driver_.quit()
+        # part of method driver. The function ends when the indentation becomes smaller
+        # Actions passed to addfinalizer get executed after a test method completes
         request.addfinalizer(quit)
-        return driver
+        return driver_
 
+    # Our test method starts with the word (that's how pytest knows it's a test)
+    # driver method is passed in(remember it returns driver_ a browser instance)
     def test_valid_credentials(self, driver):
         driver.get("http://the-internet.herokuapp.com/login")
         driver.find_element(By.ID, "username").send_keys("tomsmith")
