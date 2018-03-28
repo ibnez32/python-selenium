c10d012de9d1c223b33c4e8de6a1777815fc9295
Ibne Zabed <ibnezabed@Ibnes-MBP.T-mobile.com>
2018-03-27 21:15:07 -0400

    Page Object for loginpage

--- python-selenium/tests/login_test.py
+++ python-selenium/tests/login_test.py
@@ -1,7 +1,7 @@
-import os
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from pages import login_page
 
 class TestLogin():
     # Decorators are a form of metadata. Adds additional functionality in your tests. Used for test setup/teardown
@@ -10,7 +10,7 @@ class TestLogin():
     @pytest.fixture()
     # self is a required parameter for class methods
     # request is a parameter made available to fixtures. It enables access to loads of things during a test run
-    def driver(self, request):
+    def login(self, request):
         #_geckodriver = os.path.join(os.getcwd(), 'vendor', 'geckodriver')
         #driver_ = webdriver.Firefox(executable_path=_geckodriver)
         driver_ = webdriver.Firefox(executable_path='/Users/ibnezabed/Desktop/selenium-guidebook-python-practice/python-selenium/vendor/geckodriver')
@@ -20,14 +20,10 @@ class TestLogin():
         # part of method driver. The function ends when the indentation becomes smaller
         # Actions passed to addfinalizer get executed after a test method completes
         request.addfinalizer(quit)
-        return driver_
+        return login_page.LoginPage(driver_)
 
     # Our test method starts with the word (that's how pytest knows it's a test)
     # driver method is passed in(remember it returns driver_ a browser instance)
-    def test_valid_credentials(self, driver):
-        driver.get("http://the-internet.herokuapp.com/login")
-        driver.find_element(By.ID, "username").send_keys("tomsmith")
-        driver.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
-        driver.find_element(By.CSS_SELECTOR, "button").click()
-        assert(driver.find_element(By.CSS_SELECTOR, ".flash.success").is_displayed())
-
+    def test_valid_credentials(self, login):
+        login.with_("tomsmith", "SuperSecretPassword!")
+        assert login.success_message_present()
